4ec05a89d666bde4ac48c07d7ee714c8005cd01e
Pritam Das <pritamd47@gmail.com>
2022-04-19 16:29:04 -0700

    added git and conda environment creation options

--- initscience.sh
+++ initscience.sh
@@ -1,4 +1,4 @@
-# Usage: bash initscience.sh <project_name>
+# Usage: bash initscience.sh <project_name> <shell>
 # version 0.1: By default add the date, future version will have option to disable having date
 #              before the project name.
 #
@@ -12,12 +12,33 @@
 #    |- results/
 
 PROJNAME=$(date +%Y_%m_%d)-$1
+echo "Making the project directory - $PROJNAME"
 mkdir $PROJNAME
 cd $PROJNAME
+
+echo "Making the project directory structure"
 mkdir bin
 mkdir data
 mkdir secrets
 mkdir src
 mkdir notebooks
 mkdir results
+
+echo "Initializing git"
+git init
+touch .gitignore
+echo "./.condaenv/" >> .gitignore
+
+
+echo "Initializing environment"
+conda create --prefix "./.condaenv" -c conda-forge mamba -y
+source ./.condaenv/bin/activate ./.condaenv
+conda config --add channels defaults     # https://www.biostars.org/p/9461665/
+conda config --add channels conda-forge
+conda install conda-build -y
+conda develop ./src/
+mamba update mamba -y
+mamba install numpy matplotlib xarray pandas rasterio -y
+
+
 echo "Initialized $PROJNAME - Science Away!!"
