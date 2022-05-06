7086fef8ffd4df30482766e5f9cc3c4b100ebab5
Pritam Das <pritamd47@gmail.com>
2022-05-06 11:07:19 -0700

    optionally create local environment

--- initscience.sh
+++ initscience.sh
@@ -1,4 +1,6 @@
-# Usage: bash initscience.sh <project_name> <shell>
+# Usage: bash initscience.sh <project_name> [--local_env]
+# version 0.2: --local_env flag can be used to initialize a local environment with science-y
+#              packages preinstalled
 # version 0.1: By default add the date, future version will have option to disable having date
 #              before the project name.
 #
@@ -10,8 +12,15 @@
 #    |- src/
 #    |- notebooks/
 #    |- results/
+# If --local_env is passed, a local environment will be created inside the project directory
+#    |- .condaenv/
+#  * with `mamba`, `matplotlib`, `numpy`, `xarray`, `pandas`, and `rasterio` pre-installed. 
+#  * conda-forge will be added as a channel
+#  * the src/ directory will be added to conda path, so packages defined inside can be imported
+#    when the environment is activated
 
 PROJNAME=$(date +%Y_%m_%d)-$1
+
 echo "Making the project directory - $PROJNAME"
 mkdir $PROJNAME
 cd $PROJNAME
@@ -29,16 +38,18 @@ git init
 touch .gitignore
 echo ".condaenv/" >> .gitignore
 
-
-echo "Initializing environment"
-conda create --prefix "./.condaenv" -c conda-forge mamba -y
-source ./.condaenv/bin/activate ./.condaenv
-conda config --add channels defaults     # https://www.biostars.org/p/9461665/
-conda config --add channels conda-forge
-conda install conda-build -y
-conda develop ./src/
-mamba update mamba -y
-mamba install numpy matplotlib xarray pandas rasterio -y
-
+if [ "$2" = "--local_env" ]; then
+    echo "Initializing local environment"
+    conda create --prefix "./.condaenv" -c conda-forge mamba -y
+    source ./.condaenv/bin/activate ./.condaenv
+    conda config --add channels defaults     # https://www.biostars.org/p/9461665/
+    conda config --add channels conda-forge
+    conda install conda-build -y
+    conda develop ./src/
+    mamba update mamba -y
+    mamba install numpy matplotlib xarray pandas rasterio -y
+else
+    echo "Not creating local environment"
+fi
 
 echo "Initialized $PROJNAME - Science Away!!"
